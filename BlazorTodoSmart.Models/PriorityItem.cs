0b82d0d93d107d1fc79b660810adfe5b235351d6
diegopizzoli77 <48206916+diegopizzoli77@users.noreply.github.com>
2021-11-12 15:16:44 +0100

    Added Priority icon

---
+++ BlazorTodoSmart.Models/PriorityItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTodoSmart.Models
+{
+    public class PriorityItem
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public string IconName { get; set; }    
+
+        public string IconClass { get; set; }
+    }
+}
---
+++ BlazorTodoSmart.UI/Component/PriorityDropDown.razor.cs
@@ -0,0 +1,37 @@
+﻿using BlazorTodoSmart.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorTodoSmart.UI.Component
+{
+    public partial class PriorityDropDown
+    {
+        [Parameter]
+        public int Priority { get; set; } = 0;
+
+        private IEnumerable<PriorityItem> priorityItems = new List<PriorityItem>
+        {
+            new PriorityItem { Id=0, Description="None", IconName="flag-outline", IconClass=""},
+            new PriorityItem { Id=1, Description="Urgent", IconName="flag", IconClass="text-danger"},
+             new PriorityItem { Id=2, Description="Medium", IconName="flag", IconClass="text-warning"},
+              new PriorityItem { Id=3, Description="Normal", IconName="flag", IconClass="text-success"}
+        };
+
+        [Parameter]
+        public EventCallback<int> PriorityChanged { get; set; }
+
+
+        async Task PriorityOnChangeHandler(object theUserInput)
+        {
+            int valueSel = (int)theUserInput;
+
+            await PriorityChanged.InvokeAsync(valueSel);
+
+        }
+
+    }
+}
